0a3da8df3a163be5ee5e709d40e5a0caabb86457
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2017-03-30 18:57:15 -0700

    adds GoogleQueryService.js

---
+++ multican/www/js/services/GoogleQueryService.js
@@ -0,0 +1,58 @@
+/*global require, define, console, document, angular*/
+
+// require(['libs/MultiCanvas']);
+
+define([
+    'app'
+], function (app) {
+    'use strict';
+    var googleQueryDct = {};
+
+    app.factory("GoogleQueryService", function ($rootScope) {
+
+        googleQueryDct.rootScope = $rootScope;
+        var getRootScope = function () {
+            return googleQueryDct.rootScope;
+        },
+            getQueryDestinationDialogScope = function () {
+                var elemID = 'DestWndDialogNode',
+                    e = document.getElementById(elemID),
+                    scope = angular.element(e).scope();
+                return scope;
+            },
+
+            getPusherDialogScope = function () {
+                var elemID = 'PusherChannelDialog',
+                    e = document.getElementById(elemID),
+                    scope = angular.element(e).scope();
+                return scope;
+            },
+
+            setDialogVisibility = function (tf) {
+                var e = document.getElementById('Verbage'),
+                    scope = angular.element(e).scope(),
+                    previousVisibility = scope.VerbVis;
+                scope.VerbVis = tf ? 'flex' : 'none';
+                if (tf === false) {
+                    angular.element(e).css({'display' : 'none'});
+                }
+                return previousVisibility;
+            },
+
+            getQueryDct = function () {
+                return googleQueryDct;
+            },
+            setQuery = function (q) {
+                // alert('setQuery' + q);
+                googleQueryDct.query = q;
+            };
+        return {
+            getQueryDct: getQueryDct,
+            setQuery : setQuery,
+            getQueryDestinationDialogScope : getQueryDestinationDialogScope,
+            getPusherDialogScope : getPusherDialogScope,
+            setDialogVisibility : setDialogVisibility,
+            getRootScope : getRootScope
+        };
+    });
+});

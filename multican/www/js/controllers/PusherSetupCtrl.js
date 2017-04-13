46d216f8d8005920ee77400a2a75db4a0702a993
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2017-04-12 17:35:04 -0700

    Ready to merge to master

--- multican/www/js/controllers/PusherSetupCtrl.js
+++ multican/www/js/controllers/PusherSetupCtrl.js
@@ -94,7 +94,7 @@
                 // pusher = new Pusher(APP_KEY);
                 pusher = new Pusher(APP_KEY, {
                     authTransport: 'jsonp',
-                    authEndpoint: 'http://1ff80800.ngrok.io/pusher/auth', //'http://linkr622-arcadian.rhcloud.com/',
+                    authEndpoint: 'http://4d1aa2ec.ngrok.io/pusher/auth', //'http://linkr622-arcadian.rhcloud.com/',
                     clientAuth: {
                       key: APP_KEY,
                       secret: APP_SECRET,
